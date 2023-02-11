c5dd6ab11732736fc2b8376cb68ad148042fc2e9
Mark The Magnificent <marco.negrete@ingenieria.unam.edu>
2023-02-10 20:00:50 -0600

    Add files for practice 01

---
+++ catkin_ws/src/students/scripts/practice01.py
@@ -0,0 +1,14 @@
+#
+# COMPUTER VISION FOR ROBOTICS - FI-UNAM - 2023-2
+# PRACTICE 01 - THE OPENCV LIBRARY
+#
+
+import cv2
+import numpy as np
+
+def main():
+    print("Practice 01")
+
+if __name__ == '__main__':
+    main()
+
