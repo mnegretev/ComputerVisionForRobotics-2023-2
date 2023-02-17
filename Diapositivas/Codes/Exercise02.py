fced1271af404f37a476cbbed9dd4c52b374f0c8
Lap Jusina <generic@generic.com>
2023-02-17 12:23:17 -0600

    Update exercises

--- Diapositivas/Codes/Exercise02.py
+++ Diapositivas/Codes/Exercise02.py
@@ -2,13 +2,16 @@ import cv2
 import numpy
 
 def main():
-    img_blank  = numpy.zeros((512, 512, 3), numpy.uint8)
+    img_blank  = numpy.zeros((519, 678, 3), numpy.uint8)
     img_blank[128:256,0:256] = 255*numpy.ones((128, 256, 3), numpy.uint8)
-    img_baboon = cv2.imread('baboon.jpg')
+    img_baboon = cv2.imread('gui.png')
+    print(img_baboon.shape)
     img_and    = cv2.bitwise_and(img_baboon, img_blank)
     img_or     = cv2.bitwise_or (img_baboon, img_blank)
     img_not    = cv2.bitwise_not(img_baboon)
     img_xor    = cv2.bitwise_xor(img_baboon, img_blank)
+    cv2.imshow("Original BGR", img_baboon)
+    cv2.imshow("Mask", img_blank)
     cv2.imshow("OR Operator", img_or)
     cv2.imshow("XOR Operator", img_xor)
     cv2.imshow("AND Operator", img_and)
