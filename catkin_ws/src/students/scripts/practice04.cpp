536fb73e4d0185b1a543fc59f408927bd4e37496
sergioar3143 <sergioar3143@github.com>
2023-03-16 20:42:38 -0600

    Se pueden observar las esquinas pero las constantes pueden mejorar

--- catkin_ws/src/students/scripts/practice04.cpp
+++ catkin_ws/src/students/scripts/practice04.cpp
@@ -8,16 +8,16 @@
 using namespace cv;
 using namespace std;
 
+int umbral=1;
 int k_size = 5; //Variable kernel size for the gaussian filter
-int t1=5; // lower threshold
-int t2=10; // upper threshold
-double pi = 3.14159265358979323846;
+int t1=41; // lower threshold
 double delta=0;
 int ddepth=-1;
-int hough_threshold = 100;
+float harris_avg=0;
 
 cv::Mat get_sobel_x_gradient(cv::Mat& img){
-    float Gx[3][3]={-1/8.0,0,1/8.0,-2/8.0,0,2/8.0,-1/8.0,0,1/8.0};
+    float Gx[3][3]={-1,0,1,-2,0,2,-1,0,1};
+    //float Gx[3][3]={-1/8.0,0,1/8.0,-2/8.0,0,2/8.0,-1/8.0,0,1/8.0};
     cv::Mat SobelX=Mat(3, 3, CV_32F, &Gx);
     cv::Mat img_filter;
     //cout<<SobelX<<endl;
@@ -27,7 +27,8 @@ cv::Mat get_sobel_x_gradient(cv::Mat& img){
 }
 
 cv::Mat get_sobel_y_gradient(cv::Mat& img){
-    float Gy[3][3]={-1/8.0,-2/8.0,-1/8.0,0,0,0,1/8.0,2/8.0,1/8.0};
+    float Gy[3][3]={-1,-2,-1,0,0,0,1,2,1};
+    //float Gy[3][3]={-1/8.0,-2/8.0,-1/8.0,0,0,0,1/8.0,2/8.0,1/8.0};
     cv::Mat SobelY=Mat(3, 3, CV_32F, &Gy);
     cv::Mat img_filter;
     //cout<<SobelY<<endl;
@@ -66,7 +67,6 @@ cv::Mat Eigen_values(cv::Mat &C){
 }
 
 cv::Mat get_harris_response(cv::Mat& lambda, float k){
-    float max=-10000;
     cv::Mat R = cv::Mat(lambda.rows, lambda.cols, CV_32FC1);
     for(size_t i=0; i < lambda.rows; i++)
         for(size_t j=0; j < lambda.cols; j++)
@@ -74,11 +74,37 @@ cv::Mat get_harris_response(cv::Mat& lambda, float k){
             float l1 = lambda.at<cv::Vec2f>(i,j)[0];
             float l2 = lambda.at<cv::Vec2f>(i,j)[1];
             R.at<float>(i,j) = l1*l2 - k*(l1+l2)*(l1+l2);
-            if (R.at<float>(i,j)>max)
-                max=R.at<float>(i,j);
-        }
-    cout<<max<<endl;
+            //if( R.at<float>(i,j) != R.at<float>(i,j)) //Check if the value is nan, nan has the property to always be false
+                continue;
+        }//ventana tamaño 13
     return R.clone();
+}
+
+cv::Mat non_max_supress(cv:: Mat& R, int window_size){
+    int w=window_size/2;
+    cv::Mat W=R.clone();
+    for(size_t i=w; i<R.rows-w; i++)
+        for(size_t j=w; j<R.cols-w; j++){
+            float max=-10000000;
+            for(int k1=-w; k1<=w; k1++)
+                for(int k2=-w; k2<=w; k2++){
+                    if(R.at<float>(i+k1, j+k2) > max)
+                        max=R.at<float>(i+k1,j+k2);
+                }
+            if(R.at<float>(i,j)==max && max >harris_avg)
+                W.at<float>(i,j)=255.0;
+            else
+                W.at<float>(i,j)=0.0;
+        }
+    W.convertTo(W,CV_8UC1);
+    return W.clone();
+
+}
+
+void draw_corners(cv::Mat img, std::vector<cv::Point> corners)
+{
+    for(size_t i=0; i< corners.size(); i++)
+        cv::circle(img, corners[i], 5, cv::Scalar(0,0,255), -1);
 }
 
 
@@ -89,17 +115,17 @@ int main(int, char**)
     cv::Mat frame,Gm,G,Ga,gray,img_filter,T;
     cv::Mat SobelX,SobelY,kernel,F, Hough;
     //cout<<Arr<<endl;
-    float sigma=1.1;
     //--- INITIALIZE VIDEOCAPTURE
     VideoCapture cap;
     // open the default camera using default API
     cap.open(0);
-    cap.set(CAP_PROP_FRAME_WIDTH, 640);//Setting the width of the video
-    cap.set(CAP_PROP_FRAME_HEIGHT, 480);//Setting the height of the video//
+    cap.set(CAP_PROP_FRAME_WIDTH, 320);//Setting the width of the video
+    cap.set(CAP_PROP_FRAME_HEIGHT, 240);//Setting the height of the video//
     cv::namedWindow("Original");
-    cv::createTrackbar("Umbral de Harris:", "Original", &t1, 30, on_threshold_changed);
-    cv::createTrackbar("Ventana:", "Original", &k_size, 30, on_threshold_changed);
-    cv::createTrackbar("Umbral número de votos:", "Original", &hough_threshold, 255, on_threshold_changed);
+    cv::createTrackbar("k", "Original", &t1, 100, on_threshold_changed);
+    cv::createTrackbar("Tamaño de Ventana", "Original", &k_size, 20, on_threshold_changed);
+    cv::createTrackbar("Umbral", "Original", &umbral, 640, on_threshold_changed);
+    cv::setTrackbarMin("k", "Original", 40);
     // check if we succeeded
     if (!cap.isOpened()) {
         cerr << "ERROR! Unable to open camera\n";
@@ -117,8 +143,8 @@ int main(int, char**)
             cerr << "ERROR! blank frame grabbed\n";
             break;
         }
+        harris_avg=float(umbral);
         gray = frame.clone();
-        //gray=gray/255.0;
         cv::cvtColor(gray, gray, cv::COLOR_BGR2GRAY);
         // Initialize arguments for the filter
         gray.convertTo(gray,CV_32FC1); //Change the class type in order to work with float numbers
@@ -127,10 +153,15 @@ int main(int, char**)
         cv::Mat E=Eigen_values(C);
         //E/=(255.0*255.0);
         cv::Mat R=get_harris_response(E, t1/1000.0);
+        cv:Mat W=non_max_supress(R, k_size);
+        std::vector<cv::Point> corners;
+        cv::findNonZero(W, corners);
+        draw_corners(frame, corners);
         //R=R/(255.0*255.0*255.0);
         imshow("Original",frame);
         imshow("Covarianza", C);
-        imshow("Eigen", R);
+        imshow("Final", W);
+        cout<<harris_avg<<endl;
         //imshow("F",F);
         if (waitKey(30) == 27)
             break;
